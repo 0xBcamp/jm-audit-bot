b41aeac455b93c79b8a775979dc8426425fb138d
Jude Joubert <joubertjude@gmail.com>
2024-07-17 17:46:31 -0500

    Fix CORS problem with API on Firefox

--- api/main.py
+++ api/main.py
@@ -1,22 +1,33 @@
 import flask
 import requests
+from flask_cors import CORS
 
 app = flask.Flask(__name__)
+CORS(app)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
     return flask.jsonify({
         'msg': 'Hello, World!'
     })
 
-@app.route('/tokens/<address>', methods=['GET', 'POST'])
-def getReport(address):
+@app.route('/audit', methods=['GET'])
+def getReport():
+    address = flask.request.args.get('address')
+    if not address:
+        return flask.jsonify({
+            'error': 'Address is required'
+        })
+
     response = {
         'address': address,
     }
 
-    r = requests.get(f"https://explorer.mode.network/api/v2/smart-contracts/{address}")
+    # r = requests.get(f"https://explorer.mode.network/api/v2/smart-contracts/{address}")
+
 
     return flask.jsonify(response)
+
+
 
 app.run(host='0.0.0.0', port=8000, debug=True)
